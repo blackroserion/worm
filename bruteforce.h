42429c19d85d9e0c23a6bb9dd69dbcf54faf5042
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2016-11-26 16:21:36 -0200

    Change MAX_THREADS to prevent connection blocking
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- bruteforce.h
+++ bruteforce.h
@@ -12,7 +12,7 @@
 
 #define MAX_PASSWORD_LENGTH    8
 #define FTP_LOGIN_TRIES        5
-#define MAX_THREADS            60
+#define MAX_THREADS            40
 
 int bruteforce(char *dest, unsigned int maxlength);
 int ftp_try_login(const char *address, unsigned int port, const char *user, const char *password, int thread_id);
